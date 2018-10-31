6a75c7daeebc446b54d27244e11a476bb2e234fa
Daniel <d.r.hinchev@gmail.com>
2018-10-31 15:15:45 +0200

    minify css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './js/registerServiceWorker';
-import './styles/App.css';
+import './styles/app.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
